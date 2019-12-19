6e23278b23072becd4f8e3cd23e393c2d7d4a304
Carolina Alves <carol.alves99@live.com>
2019-12-19 15:21:21 -0300

    FindById implementation

--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -1,11 +1,25 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 
 public class SellerDaoJdbc implements SellerDao {
+
+  private Connection connection;
+
+  public SellerDaoJdbc(Connection connection) {
+    this.connection = connection;
+  }
 
   @Override
   public void insert(Seller seller) {
@@ -24,7 +38,40 @@ public class SellerDaoJdbc implements SellerDao {
 
   @Override
   public Seller findById(Integer id) {
-    return null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    try {
+      preparedStatement = connection.prepareStatement(
+          "SELECT seller.*, department.Name AS DepName\n"
+              + "FROM seller INNER JOIN department\n"
+              + "ON seller.DepartmentId = department.Id\n"
+              + "WHERE seller.Id = ?");
+      preparedStatement.setInt(1, id);
+      resultSet = preparedStatement.executeQuery();
+      if (resultSet.next()) {
+        Department department = new Department();
+        department.setId(resultSet.getInt("DepartmentId"));
+        department.setName(resultSet.getString("DepName"));
+
+        Seller seller = new Seller();
+        seller.setId(resultSet.getInt("Id"));
+        seller.setName(resultSet.getString("Name"));
+        seller.setEmail(resultSet.getString("Email"));
+        seller.setBaseSalary(resultSet.getDouble("BaseSalary"));
+        seller.setBirthDate(resultSet.getDate("BirthDate"));
+        seller.setDepartment(department);
+        return seller;
+      }
+
+      return null;
+
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeStatement(preparedStatement);
+      DB.closeResultSet(resultSet);
+    }
+
   }
 
   @Override
